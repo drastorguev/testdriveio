ff7b835a4b0b4de7d7284eb49f9b8ca9f81c8a2f
Dmitry Rastorguev <dmitry@rastorguev.co.uk>
2018-02-01 18:55:23 +0000

    part 1 done

--- users-service/project/__init__.py
+++ users-service/project/__init__.py
@@ -1,19 +1,29 @@
 # users-service/project/__init__.py
 
 
+import os
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 
-# instantiate the app
-app = Flask(__name__)
-
-# set config
-app.config.from_object('project.config.DevelopmentConfig')
+# instantiate the db
+db = SQLAlchemy()
 
 
-@app.route('/users/ping', methods=['GET'])
-def ping_pong():
-    return jsonify({
-        'status': 'success',
-        'message': 'pong!'
-    })
+def create_app():
+
+    # instantiate the app
+    app = Flask(__name__)
+
+    # set config
+    app_settings = os.getenv('APP_SETTINGS')
+    app.config.from_object(app_settings)
+
+    # set up extensions
+    db.init_app(app)
+
+    # register blueprints
+    from project.api.users import users_blueprint
+    app.register_blueprint(users_blueprint)
+
+    return app
